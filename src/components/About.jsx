38f59befb08299c246e6d18618234b0512aea241
Andres-Betancur-Castañeda <ac20061234@gmail.com>
2025-11-05 15:40:46 -0500

    feat: componente Perfil con descripción personalizada

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,19 @@ export default function About() {
     <section>
       <h2>Sobre mí</h2>
       <p>
-        Soy un desarrollador apasionado por la tecnología y la creación de
-        interfaces atractivas con React y Vite.
+        Soy un estudiante del SENA, actualmente cursando el programa de
+        Desarrollo de Software. Me apasiona el desarrollo web y
+        estoy enfocado en aprender y mejorar mis habilidades en React y demas, he 
+        aprendido a trabajar en proyectos individuales y colaborativos, lo que me ha
+        permitido desarrollar habilidades de trabajo en equipo y comunicación, En estos trabajos he aprendido 
+        a utilizar heraamientas como git y github, y a trabajar con html y css.
       </p>
+      <ul>
+        <li>React</li>
+        <li>JavaScript</li>
+        <li>HTML & CSS</li>
+        <li>Git y GitHub</li>
+      </ul>
     </section>
   );
 }
